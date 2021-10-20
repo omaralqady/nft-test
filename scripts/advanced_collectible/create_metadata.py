44b08aaa438fbdaa1d922da78b5a94e07aea012d
Omar Alqady <omar@founda.com>
2021-10-20 23:07:37 +0200

    feat: upload to local ipfs node

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -2,6 +2,9 @@ from brownie import AdvancedCollectible, network
 from scripts.utils import get_breed
 from metadata.sample_metadata import metadata_template
 from pathlib import Path
+import requests
+
+ipfs_localhost = "http://127.0.0.1:5001"
 
 
 def main():
@@ -21,3 +24,18 @@ def main():
             print(f"Creating metadata file: {metadata_file_name}")
             collectibe_metadata["name"] = breed
             collectibe_metadata["description"] = f"A cute {breed} puppy"
+            image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
+            image_uri = upload_to_ipfs(image_path)
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        endpoint = ipfs_localhost + "/api/v0/add"
+
+        res = requests.post(endpoint, files={"file": image_binary})
+        ipfs_hash = res.json()["Hash"]
+        filename = filepath.split("/")[-1:][0]
+        image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+        print(image_uri)
+        return image_uri
